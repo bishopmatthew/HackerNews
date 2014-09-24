f692791d83e7e578398b24c51ba22f45d68e52d9
Matthew Bishop <bish.mat@gmail.com>
2014-09-24 13:55:06 -0600

    Fix login error

--- src/com/airlocksoftware/hackernews/data/LoginManager.java
+++ src/com/airlocksoftware/hackernews/data/LoginManager.java
@@ -3,34 +3,33 @@ package com.airlocksoftware.hackernews.data;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
-import org.jsoup.nodes.Document;
 
 public class LoginManager {
 
-	private static final String LOGIN_URL_EXTENSION = "/newslogin?whence=news";
+	private static final String LOGIN_URL_EXTENSION = "/login?whence=news";
 
 	/**
 	 * Attempts to log the user into http://news.ycombinator.com. If successful, returns a user authentication cookie.
 	 * Else it returns null.
-	 **/
+	 */
 	public static String login(String username, String password) {
 		try {
-			Response loginResponse = ConnectionManager.anonConnect(LOGIN_URL_EXTENSION)
-																								.method(Method.GET)
-																								.execute();
-			Document loginPage = loginResponse.parse();
-			String fnid = loginPage.select("input[name=fnid]")
-															.attr("value");
+//			Response loginResponse = ConnectionManager.anonConnect(LOGIN_URL_EXTENSION)
+//					.method(Method.GET)
+//					.execute();
+//			Document loginPage = loginResponse.parse();
+//			String fnid = loginPage.select("input[name=fnid]")
+//					.attr("value");
 
-			Response response = ConnectionManager.anonConnect("/y")
-																						.data("fnid", fnid)
-																						.data("u", username)
-																						.data("p", password)
-																						.header("Origin", ConnectionManager.BASE_URL)
-																						.followRedirects(true)
-																						.referrer(ConnectionManager.BASE_URL + LOGIN_URL_EXTENSION)
-																						.method(Method.POST)
-																						.execute();
+			Response response = ConnectionManager.anonConnect("/login")
+					.data("whence", "news")
+					.data("acct", username)
+					.data("pw", password)
+					.header("Origin", ConnectionManager.BASE_URL)
+					.followRedirects(true)
+					.referrer(ConnectionManager.BASE_URL + LOGIN_URL_EXTENSION)
+					.method(Method.POST)
+					.execute();
 
 			String cookie = response.cookie("user");
 			if (StringUtils.isNotBlank(cookie)) {
